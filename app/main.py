c028c1a2d81f8b854979b5dd3714ffd02ad0588d
Deskent <battenetciz@gmail.com>
2022-12-12 18:57:52 +0300

    v 1.0
    fix logging

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 
-import requests
 from flask import Flask, request
 
 import pushapi
@@ -9,27 +8,6 @@ from config import settings, logger
 from sender import TrafficMonitor, ExampleChatMsg, ExampleDescription, DemoSkypePerson
 
 app = Flask(__name__)
-
-
-
-def send_message_to_user(message: str) -> None:
-    """
-    The function of sending the message with the result of the script
-    """
-    telegram_id = settings.TELEGRAM_ID
-    if not telegram_id or not settings.TELEBOT_TOKEN:
-        logger.debug("Can`t send report to telegram: no token or ID")
-        return
-    url: str = (
-        f"https://api.telegram.org/bot{settings.TELEBOT_TOKEN}/sendMessage?"
-        f"chat_id={telegram_id}"
-        f"&text={message}"
-    )
-    try:
-        requests.get(url, timeout=5)
-        logger.debug(f"telegram id: {telegram_id}\n message: {message}")
-    except Exception as err:
-        logger.debug(f"telegram id: {telegram_id}\n message: {message}\n requests error: {err}")
 
 
 def create_message_instance(data: dict, text: str = '') -> ExampleChatMsg:
@@ -60,7 +38,6 @@ def get_message_event(data: dict, text: str = '') -> ExampleDescription:
         data_attrs=None,  # данные для события класса kChat передаются в списке messages
         messages=[message]  # сообщения чата
     )
-    logger.debug(f"Getting message event: OK\n{message_event}")
 
     return message_event
 
@@ -80,6 +57,7 @@ def get_message(data: dict) -> str:
     owner: str = data.get('owner', "Owner error")
     date_time: datetime = datetime.fromtimestamp(data.get('datetime'))
     return (
+        "В OwnCloud загружен файл:\n"
         f"Владелец: {owner}\n"
         f"Имя файла: {file_name}\n"
         f"Размер файла (bytes): {data.get('size')}\n"
@@ -108,9 +86,6 @@ def get_hook():
             try:
                 data: dict = request.json
                 logger.debug(data)
-                # TODO удалить в проде:
-                send_message_to_user(message=str(data))
-                # *****
                 message_event: ExampleDescription = get_message_event(data, text)
                 send_message_to_traffic_monitor(message_event)
                 logger.debug("Message sent: OK")
